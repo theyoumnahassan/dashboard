b7814d4499a19bc7c31f2605dd1a47fa8af439e9
theyoumnahassan <128295189+theyoumnahassan@users.noreply.github.com>
2023-09-05 14:08:33 +0400

    Update Overall.py

--- pages/Overall.py
+++ pages/Overall.py
@@ -1,63 +1,70 @@
-
 import streamlit as st
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
 import pandas as pd
 import altair as alt
-from datetime import datetime, timedelta
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
-# Load credentials
-creds = Credentials.from_authorized_user_file("credentials.json")
+# Load Google Analytics credentials
+credentials = Credentials.from_authorized_user_file('credentials.json')
+analytics = build('analyticsreporting', 'v4', credentials=credentials)
 
-# Initialize the Google Analytics Reporting API
-service = build("analyticsreporting", "v4", credentials=creds)
+# Define the Google Analytics report request
+report_request = {
+    'viewId': 'properties/254216168',
+    'dateRanges': [{'startDate': '2022-01-01', 'endDate': 'today'}],
+    'metrics': [
+        {'expression': 'ga:users'},
+        {'expression': 'ga:screenviews'},
+        {'expression': 'ga:sessions'},
+        {'expression': 'ga:engagedSessions'}
+    ]
+}
 
-# Streamlit UI
-st.title("Asharq Now Analytics")
+response = analytics.reports().batchGet(body={'reportRequests': [report_request]}).execute()
 
-# Datepicker for selecting the date range
-st.sidebar.write("Select Date Range:")
-start_date = st.sidebar.date_input("Start Date", datetime.now() - timedelta(days=7))
-end_date = st.sidebar.date_input("End Date", datetime.now())
+# Extract data from the response
+data = response['reports'][0]['data']['rows'][0]['metrics'][0]['values']
 
-# Fetch Data using the Reporting API
-response = service.reports().batchGet(
+# Create Streamlit app
+st.title('Google Analytics Report')
+
+# Display metrics cards
+st.subheader('Metrics')
+metric_names = ['Total Users', 'Screen Page Views', 'Sessions', 'Engaged Sessions']
+for i, metric_name in enumerate(metric_names):
+    st.metric(label=metric_name, value=data[i])
+
+# Date picker
+st.subheader('Date Range')
+start_date = st.date_input('Start Date', pd.to_datetime('2022-01-01'))
+end_date = st.date_input('End Date', pd.to_datetime('today'))
+
+# Bar graph using Altair
+st.subheader('Bar Graph: Page Views by Page Title')
+query_response = analytics.reports().batchGet(
     body={
-        "reportRequests": [
-            {
-                "viewId": "254216168",  # Replace with your view ID
-                "dateRanges": [{"startDate": start_date.strftime("%Y-%m-%d"), "endDate": end_date.strftime("%Y-%m-%d")}],
-                "metrics": [{"expression": "ga:sessions"}, {"expression": "ga:screenPageViews"}, {"expression": "ga:engagementRate"}, {"expression": "ga:totalUsers"}],
-                "dimensions": [{"name": "ga:screenPageViews"}],
-                "orderBys": [{"fieldName": "ga:screenPageViews", "sortOrder": "DESCENDING"}],
-            }
-        ]
+        'reportRequests': [{
+            'viewId': 'properties/254216168',
+            'dateRanges': [{'startDate': start_date.strftime('%Y-%m-%d'), 'endDate': end_date.strftime('%Y-%m-%d')}],
+            'metrics': [{'expression': 'ga:screenviews'}],
+            'dimensions': [{'name': 'ga:pageTitle'}],
+            'orderBys': [{'fieldName': 'ga:screenviews', 'sortOrder': 'DESCENDING'}],
+            'pageSize': 10
+        }]
     }
 ).execute()
 
-data = response["reports"][0]["data"]["rows"]
-if data:
-    df = pd.DataFrame(
-        [(row["dimensions"][0], int(row["metrics"][0]["values"][0]), float(row["metrics"][0]["values"][1]), float(row["metrics"][0]["values"][2]), int(row["metrics"][0]["values"][3])) for row in data],
-        columns=["Screen", "Sessions", "Page Views", "Engagement Rate", "Total Users"],
-    )
+chart_data = []
+for row in query_response['reports'][0]['data']['rows']:
+    chart_data.append({'Page Title': row['dimensions'][0], 'Screen Page Views': int(row['metrics'][0]['values'][0])})
 
-    # Display cards with total metrics
-    st.sidebar.write("Overall Metrics:")
-    st.sidebar.write(f"Total Sessions: {df['Sessions'].sum()}")
-    st.sidebar.write(f"Total Screen Page Views: {df['Page Views'].sum()}")
-    st.sidebar.write(f"Average Engagement Rate: {df['Engagement Rate'].mean():.2f}%")
-    st.sidebar.write(f"Total Users: {df['Total Users'].sum()}")
+chart_df = pd.DataFrame(chart_data)
+chart = alt.Chart(chart_df).mark_bar(color='#c41b1a').encode(
+    x='Screen Page Views',
+    y=alt.Y('Page Title', sort='-x')
+).properties(
+    width=600,
+    height=300
+)
 
-    # Display Bar Chart using Altair
-    st.write("Page Views by Screen")
-    chart = alt.Chart(df).mark_bar(color='#c41b1a').encode(
-        x=alt.X('Page Views:Q', title='Page Views', axis=alt.Axis(grid=False, titlePadding=20, labelPadding=10)),
-        y=alt.Y('Screen:O', title='Screen Name', sort='-x', axis=alt.Axis(labelOffset=10)),
-        tooltip=['Screen', 'Page Views']
-    ).properties(width=1200)  # Increase chart width for better readability
-
-    st.altair_chart(chart, use_container_width=True)
-else:
-    st.write("No data available")
+st.altair_chart(chart, use_container_width=True)
