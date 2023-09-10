765a8813b0f7c800b68d483ff5c83310ee5ba1bf
theyoumnahassan <128295189+theyoumnahassan@users.noreply.github.com>
2023-09-10 15:04:07 +0400

    Update google_analytics_dashboard.py

--- google_analytics_dashboard.py
+++ google_analytics_dashboard.py
@@ -5,6 +5,7 @@ from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import RunRealtimeReportRequest
 import pandas as pd
 import altair as alt
+import openai
 
 # Load credentials
 credentials = load_credentials_from_file("./credentials.json")[0]
@@ -17,6 +18,35 @@ st.title("Asharq Now Realtime Analytics")
 
 # Add logo image
 st.sidebar.image("img.png", width=150)  # Adjust the width as needed
+#add chatbot
+with st.sidebar:
+    openai_api_key = st.text_input(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", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot") if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
+
+
+#add openai
+
 
 # Fetch Data
 request = RunRealtimeReportRequest(
