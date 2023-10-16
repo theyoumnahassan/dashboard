27bd3ffcbb7cb243b44d8a764a984e1bffb31ab6
Youmna <128295189+theyoumnahassan@users.noreply.github.com>
2023-10-17 01:47:41 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,55 @@
 import streamlit as st
+from google.oauth2 import service_account
+from google.analytics.data_v1alpha import BetaAnalyticsDataClient
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+# Set up your GA4 API credentials
+credentials = service_account.Credentials.from_service_account_info(
+    YOUR_SERVICE_ACCOUNT_INFO, scopes=["https://www.googleapis.com/auth/analytics.readonly"]
 )
 
-st.write("# Welcome to Streamlit! 👋")
+# Create a GA4 client
+client = BetaAnalyticsDataClient(credentials=credentials)
 
-st.sidebar.success("Select a demo above.")
+# Function to fetch data from the GA4 API
+def fetch_data_from_ga4_api(view_id, start_date, end_date, metrics):
+    # Make a request to the GA4 API to fetch data
+    response = client.run_report(
+        entity={"property_id": f"properties/{view_id}"},  # Use the provided View ID
+        metrics=metrics,
+        date_ranges=[{"start_date": start_date, "end_date": end_date}],
+    )
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+    # Process the API response JSON to extract the data
+    data = process_response(response)
+    return data
+
+# Define a function to process the GA4 API response
+def process_response(response):
+    # Extract data from the API response JSON
+    # Customize this part based on your specific API response structure
+    data = {}  # Create a dictionary to store your data
+    # Extract and format data as needed
+    return data
+
+# Streamlit app layout
+st.title('Google Analytics Dashboard')
+
+# User Input: GA4 View ID
+view_id = st.text_input("Enter GA4 View ID")
+
+# Date Range Picker
+start_date = st.date_input("Select Start Date")
+end_date = st.date_input("Select End Date")
+
+# Metrics Selection
+metrics = st.multiselect("Select Metrics", ["ga:pageviews", "ga:users"])
+
+# Fetch data from GA4 API when a button is clicked
+if st.button("Fetch Data"):
+    data = fetch_data_from_ga4_api(view_id, start_date, end_date, metrics)
+    
+    # Display metrics based on the data from the GA4 API
+    for metric in metrics:
+        st.metric(metric.capitalize(), data.get(metric.capitalize(), 0))  # Display metrics
+
+    # Add other components and visualizations here
